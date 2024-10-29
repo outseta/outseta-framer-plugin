20951b116f7d1abb75cfec62e29a2901c3ac08e5
raae <queen@raae.codes>
2024-10-29 14:37:17 +0100

    refactor: improve url override UI by turning it on

--- src/routes/custom-code.index.tsx
+++ src/routes/custom-code.index.tsx
@@ -1,7 +1,11 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useState, type FormEvent } from "react";
 import { useMutation } from "@tanstack/react-query";
-import { Button, TextControls } from "@triozer/framer-toolbox";
+import {
+  Button,
+  SegmentedControls,
+  TextControls,
+} from "@triozer/framer-toolbox";
 
 import { setCustomCode, useCustomCode } from "../custom-code";
 import { PageListControls } from "../common";
@@ -14,43 +18,52 @@ function CustomCode() {
   const navigate = useNavigate();
 
   const customCode = useCustomCode();
+  const [config, setConfig] = useState({
+    domain: customCode.domain,
 
-  const [domain, setDomain] = useState<string>(customCode.domain);
-  const [postLoginPath, setPostLoginPath] = useState<string>(
-    customCode.postLoginPath,
-  );
-  const [postSignupPath, setPostSignupPath] = useState<string>(
-    customCode.postSignupPath,
-  );
+    overrideLoginPath: customCode.domain
+      ? Boolean(customCode.postLoginPath)
+      : true,
+    postLoginPath: customCode.postLoginPath || "",
+
+    overrideSignupPath: customCode.domain
+      ? Boolean(customCode.postSignupPath)
+      : false, // uses message if false
+    postSignupPath: customCode.postSignupPath || "",
+  });
 
   const mutation = useMutation({
     mutationFn: setCustomCode,
     onSuccess: () => navigate({ to: "/", from: Route.fullPath }),
   });
 
-  const domainHasChanged = domain !== customCode.domain;
-  const postLoginPathHasChanged = postLoginPath !== customCode.postLoginPath;
-  const postSignupPathHasChanged = postSignupPath !== customCode.postSignupPath;
+  const domainHasChanged = config.domain !== customCode.domain;
+  const postLoginPathHasChanged =
+    config.postLoginPath !== customCode.postLoginPath;
+  const postSignupPathHasChanged =
+    config.postSignupPath !== customCode.postSignupPath;
   const nothingHasChanged =
     !domainHasChanged && !postLoginPathHasChanged && !postSignupPathHasChanged;
   const disabled =
-    !domain || nothingHasChanged || !domain.includes(".outseta.com");
+    !config.domain ||
+    nothingHasChanged ||
+    !config.domain.includes(".outseta.com");
 
   return (
     <form
       onSubmit={async (event: FormEvent) => {
         event.preventDefault();
-        if (!domain) return;
+        if (!config.domain) return;
 
-        mutation.mutate({ domain, postLoginPath, postSignupPath });
+        mutation.mutate(config);
       }}
     >
       <TextControls
         title="Outseta Domain"
         placeholder="your-domain.outseta.com"
-        value={domain}
+        value={config.domain}
         required
-        onChange={(value) => setDomain(value)}
+        onChange={(value) => setConfig({ ...config, domain: value })}
       ></TextControls>
 
       {!customCode.domain && (
@@ -66,21 +79,74 @@ function CustomCode() {
         </p>
       )}
 
-      <PageListControls
-        title="Post Login Path"
-        value={postLoginPath}
-        onChange={(value) => setPostLoginPath(value)}
+      <SegmentedControls
+        title="Post Login"
+        items={[
+          { value: false, label: "Same path" },
+          { value: true, label: "Go to path" },
+        ]}
+        value={Boolean(config.postLoginPath)}
+        onChange={(value) => {
+          if (value) {
+            setConfig({ ...config, postLoginPath: "/" });
+          } else {
+            setConfig({ ...config, postLoginPath: "" });
+          }
+        }}
       />
 
-      <PageListControls
-        title="Post Signup Path"
-        value={postSignupPath}
-        onChange={(value) => setPostSignupPath(value)}
+      {!Boolean(config.postLoginPath) && (
+        <p>
+          <small>
+            The user stays on the same page, works best with popup embeds.
+          </small>
+        </p>
+      )}
+
+      {Boolean(config.postLoginPath) && (
+        <PageListControls
+          title="Post Login Path"
+          value={config.postLoginPath}
+          onChange={(value) => setConfig({ ...config, postLoginPath: value })}
+        />
+      )}
+
+      <SegmentedControls
+        title="Post Signup"
+        items={[
+          { value: false, label: "Show message" },
+          { value: true, label: "Go to path" },
+        ]}
+        value={Boolean(config.postSignupPath)}
+        onChange={(value) => {
+          if (value) {
+            setConfig({ ...config, postSignupPath: "/" });
+          } else {
+            setConfig({ ...config, postSignupPath: "" });
+          }
+        }}
       />
+
+      {!Boolean(config.postSignupPath) && (
+        <p>
+          <small>
+            A confirmation message shows in the embed as the last step.
+          </small>
+        </p>
+      )}
+
+      {Boolean(config.postSignupPath) && (
+        <PageListControls
+          title="Post Signup Path"
+          value={config.postSignupPath}
+          onChange={(value) => setConfig({ ...config, postSignupPath: value })}
+        />
+      )}
 
       <Button variant="primary" disabled={disabled}>
         {customCode.domain ? "Update" : "Connect"}
       </Button>
+
       <div>
         <p>
           Adds the Outseta script to the site's head and pulls in data for the
