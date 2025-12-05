2dffe86118338ec1883facd7c17ca7ab1f853fe7
raae <queen@raae.codes>
2025-12-05 11:52:02 +0100

    fix(SubmitButton): update button disabled state to consider form dirtiness

--- src/forms/SubmitButton.tsx
+++ src/forms/SubmitButton.tsx
@@ -4,9 +4,9 @@ import { useFormContext } from "./form-context";
 export default function SubmitButton({ label }: { label: string }) {
   const form = useFormContext();
   return (
-    <form.Subscribe selector={(state) => state.isSubmitting}>
-      {(isSubmitting) => (
-        <Button variant="primary" type="submit" disabled={isSubmitting}>
+    <form.Subscribe selector={(state) => ({ isSubmitting: state.isSubmitting, isDirty: state.isDirty })}>
+      {({ isSubmitting, isDirty }) => (
+        <Button variant="primary" type="submit" disabled={!isDirty || isSubmitting}>
           {label}
         </Button>
       )}
