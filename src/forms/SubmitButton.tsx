1dd4d63cc4811fd19757ee6077e7ca4028c46d79
raae <queen@raae.codes>
2026-01-06 20:44:44 +0100

    feat(SubmitButton): add forceEnabled prop to control button state

--- src/forms/SubmitButton.tsx
+++ src/forms/SubmitButton.tsx
@@ -1,12 +1,22 @@
 import { Button } from "@triozer/framer-toolbox";
 import { useFormContext } from "./form-context";
 
-export default function SubmitButton({ label }: { label: string }) {
+export default function SubmitButton({
+  label,
+  forceEnabled,
+}: {
+  label: string;
+  forceEnabled?: boolean;
+}) {
   const form = useFormContext();
   return (
     <form.Subscribe selector={(state) => ({ isSubmitting: state.isSubmitting, isDirty: state.isDirty })}>
       {({ isSubmitting, isDirty }) => (
-        <Button variant="primary" type="submit" disabled={!isDirty || isSubmitting}>
+        <Button
+          variant="primary"
+          type="submit"
+          disabled={(!isDirty && !forceEnabled) || isSubmitting}
+        >
           {label}
         </Button>
       )}
